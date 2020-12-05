23abd392f0122ac53a1366ad96c6d67d7e6dd429
Omega-Zero <36581543+Omega-Zero@users.noreply.github.com>
2020-12-04 20:51:05 -0500

    every piece has its own class now and is properly returning method calls

--- src/chessGame/ChessBoard.java
+++ src/chessGame/ChessBoard.java
@@ -6,68 +6,78 @@ package chessGame;
 public class ChessBoard {
 
 	public static void main(String[] args) {
-
-		generateBoard();
 		
-	
+		generateBoard(newGame());
 	}
 
 	
-	public static void generateBoard() {
-
-		StdDraw.clear();
+	public static void generateBoard(Square[][] board) {
+ 		StdDraw.clear();
 		StdDraw.enableDoubleBuffering();
-
-		Square[][] boardArray = new Square[8][8];
 		
-		
-		
-		//cycle through every cell on board 
-		for (int column = 0; column <= 7; column++) {
-			for (int row = 0; row <= 7; row++) {
-				
-				Square currentSquare = boardArray[row][column];
-				
-				//assigns square object to each square and assigns it white/black (draw null squares)
-				if(isWhiteSpace(row, column)) {
-					currentSquare = new Square(row, column, true, new Pawn(true));
-				}else{
-					currentSquare = new Square(row, column, false, null);
-				}
-					
-				//Draws black, white squares and pieces based off obj array values
+		//cycle through every cell on board, calculate 
+		//its position on the screen and get the cell's picture
+		for (int column = 0; column < 8; column++) {
+			for (int row = 0; row < 8; row++) {
+				//calculates the *center* of each photo(piece) will go
 				double xCoord = ((double) column * (1.0/8)) + (0.5/8);
 				double yCoord = ((double) row * (1.0/8)) + (0.5/8);
-				
-				//if no piece
-				if (currentSquare.getPiece() == null) {
-					//if white
-					if (currentSquare.getSquareColor()) {
-						StdDraw.square(xCoord, yCoord, 0.5/8);
-					}else{
-						StdDraw.filledSquare(xCoord, yCoord, 0.5/8);
-					}
-					
-				//there's a piece
-				}else{
-					
-					//if whiteSquare
-					if(currentSquare.getSquareColor()) {
-						//set picture according to piece color and square color
-						currentSquare.getPiece().setPicture(true, true, xCoord, yCoord);
-					}else {
-						
-					}
-					
-				}
-		
+				//draw the picture of whatever obj in cell
+				StdDraw.picture(xCoord, yCoord, board[row][column].setPicture(), .125, .125);
 			}
-
 		}
 		StdDraw.show();
 	}// END generateBoard()
 	
 	
+	
+	public static Square[][] newGame(){
+		
+		Square[][] newGameBoard = new Square[8][8];
+		
+		//init all squares
+		for (int row = 0; row < 8; row++) {
+			for (int column = 0; column < 8; column++) {
+				//if its a white space
+				if(isWhiteSpace(row, column)) {	
+					newGameBoard[row][column] = new Square(row, column, true);
+							}else{//is black space
+					newGameBoard[row][column] = new Square(row, column, false);
+				}
+			}
+		}
+		
+		//init pieces, Kings, Queens, Rooks, Knights, Bishops, Pawns
+		//white
+		newGameBoard[0][4] = new Square(0, 4, isWhiteSpace(0, 4), new King(true));
+		newGameBoard[0][3] = new Square(0, 3, isWhiteSpace(0, 3), new Queen(true));
+		newGameBoard[0][0] = new Square(0, 0, isWhiteSpace(0, 0), new Rook(true));
+		newGameBoard[0][7] = new Square(0, 7, isWhiteSpace(0, 7), new Rook(true));
+		newGameBoard[0][1] = new Square(0, 1, isWhiteSpace(0, 1), new Knight(true));
+		newGameBoard[0][6] = new Square(0, 6, isWhiteSpace(0, 6), new Knight(true));
+		newGameBoard[0][2] = new Square(0, 2, isWhiteSpace(0, 2), new Bishop(true));
+		newGameBoard[0][5] = new Square(0, 5, isWhiteSpace(0, 5), new Bishop(true));
+		//black
+		newGameBoard[7][4] = new Square(7, 4, isWhiteSpace(7, 4), new King(false));
+		newGameBoard[7][3] = new Square(7, 3, isWhiteSpace(7, 3), new Queen(false));
+		newGameBoard[7][0] = new Square(7, 0, isWhiteSpace(7, 0), new Rook(false));
+		newGameBoard[7][7] = new Square(7, 7, isWhiteSpace(7, 7), new Rook(false));
+		newGameBoard[7][1] = new Square(7, 1, isWhiteSpace(7, 1), new Knight(false));
+		newGameBoard[7][6] = new Square(7, 6, isWhiteSpace(7, 6), new Knight(false));
+		newGameBoard[7][2] = new Square(7, 2, isWhiteSpace(7, 2), new Bishop(false));
+		newGameBoard[7][5] = new Square(7, 5, isWhiteSpace(7, 5), new Bishop(false));
+		//pawns
+		for (int column=0; column<8; column++) {
+			newGameBoard[6][column] = new Square(6, column, isWhiteSpace(6, column), new Pawn(false)); //black 
+			newGameBoard[1][column] = new Square(1, column, isWhiteSpace(1, column), new Pawn(true)); //white
+		}
+		
+		return newGameBoard;
+	}//end newGame()
+
+	
+	
+
 	//returns true if number is odd
 	public static boolean isOdd(int num) {
 		
@@ -88,7 +98,6 @@ public class ChessBoard {
 		}
 		return result;
 	}//end isWhiteSpace()
-	
 	
 	
 
--- src/chessGame/Pawn.java
+++ src/chessGame/Pawn.java
@@ -2,34 +2,49 @@ package chessGame;
 
 public class Pawn extends ChessPiece {
 
-
+	boolean firstMove = true;
 	
 	public Pawn(boolean color) {
 		super(color);
+		
 	}
 	
 
 
 	@Override
-	public boolean canMove(Object[][] arrayBoard, Square startSquare, Square startEnd) {
-		return false;
+	public boolean canMove(Object[][] arrayBoard, Square startSquare, Square endSquare) {
+			int stSqCol = startSquare.getColumn();
+			int enSqCol = endSquare.getColumn();
+			
+			//check to see if the endsquare is in the same row 
+			if(stSqCol == enSqCol) {
+				return false;
+				
+			}	
+			return true;
 	}
 	
-	public void setPicture(boolean whiteSquare, boolean whitePiece, double xCoord, double yCoord) {
+	@Override
+	public String getPicture(boolean whiteSquare, boolean whitePiece) {
 		
-		
-		
-		//If endSquare is white, pic1, else pic 2
-		if (whiteSquare == true){
-		
-			StdDraw.picture(xCoord, yCoord, "pawn.jpg", 0.125, 0.125);
-			
-		}else {
-			
-			StdDraw.picture(xCoord, yCoord, "pawn.jpg", 0.125, 0.125);
+		//if square piece is on is white
+		if (whiteSquare){
+			//if white piece
+			if (whitePiece) {
+				return "wpawnWsq.jpeg";
+			}else{//black piece
+				return "bpawnWsq.jpeg";
+			}
+	
+		}else{//square is black
+			//if white piece
+			if (whitePiece) {
+				return "wpawnBsq.jpeg";
+			}else{//black piece
+				return "bpawnBsq.jpeg";
+			}
 			
 		}
-		
 	}
 	
 	
